f852b2ae7631d2ea39ff6da011f7bc31fe9c981c
alperen gunes <alperengunes1@Alperen-MacBook-Pro.local>
2022-03-05 00:45:19 +0300

    Start

---
+++ app.tsx
@@ -0,0 +1,162 @@
+/**
+ * Welcome to the main entry point of the app. In this file, we'll
+ * be kicking off our app.
+ *
+ * Most of this file is boilerplate and you shouldn't need to modify
+ * it very often. But take some time to look through and understand
+ * what is going on here.
+ *
+ * The app navigation resides in ./app/navigators, so head over there
+ * if you're interested in adding screens and navigators.
+ */
+import "./i18n"
+import "./utils/ignore-warnings"
+import React, { useState, useEffect, useRef } from "react"
+import { NavigationContainerRef } from "@react-navigation/native"
+import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context"
+import { initFonts } from "./theme/fonts" // expo
+import * as storage from "./utils/storage"
+import Toast from 'react-native-toast-message';
+import firebase from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+import {Platform} from 'react-native';
+import {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
+import { DEFAULT_API_CONFIG } from "./services/api/api-config";
+import SplashScreen from 'react-native-splash-screen';
+
+
+
+import {
+  useBackButtonHandler,
+  RootNavigator,
+  canExit,
+  setRootNavigation,
+  useNavigationPersistence,
+} from "./navigators"
+import { RootStore, RootStoreProvider, setupRootStore } from "./models"
+import { ToggleStorybook } from "../storybook/toggle-storybook"
+
+// This puts screens in a native ViewController or Activity. If you want fully native
+// stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
+// https://github.com/kmagiera/react-native-screens#using-native-stack-navigator
+import { enableScreens } from "react-native-screens"
+import axios from "axios"
+enableScreens()
+
+export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
+
+/**
+ * This is the root component of our app.
+ */
+function App() {
+  const navigationRef = useRef<NavigationContainerRef>()
+  const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined)
+  const apiUrl = DEFAULT_API_CONFIG.url
+
+  setRootNavigation(navigationRef)
+  useBackButtonHandler(navigationRef, canExit)
+  const { initialNavigationState, onNavigationStateChange } = useNavigationPersistence(
+    storage,
+    NAVIGATION_PERSISTENCE_KEY,
+  )
+  // Kick off initial async loading actions, like loading fonts and RootStore
+  useEffect(() => {
+    ;(async () => {
+      await initFonts() // expo
+      setupRootStore().then(setRootStore)
+      SplashScreen.hide()
+      messaging()
+        .getToken(firebase.app().options.messagingSenderId)
+        .then(x => console.log(x))
+        .catch(e => console.log(e));
+    })()
+  }, [])
+
+  const registerForRemoteMessages = () => {
+    firebase
+      .messaging()
+      .registerDeviceForRemoteMessages()
+      .then(() => {
+        console.log('Registered');
+        requestPermissions();
+      })
+      .catch(e => console.log(e));
+  };
+  const requestPermissions = () => {
+    firebase
+      .messaging()
+      .requestPermission()
+      .then((status: FirebaseMessagingTypes.AuthorizationStatus) => {
+        if (status === 1) {
+          console.log('Authorized');
+          onMessage();
+        } else {
+          console.log('Not authorized');
+        }
+      })
+      .catch(e => console.log(e));
+  };
+
+  const onMessage = () => {
+    firebase.messaging().onMessage(response => {
+      showNotification(response.data!.notification);
+    });
+  };
+
+  const showNotification = (notification: any) => {
+    console.log('Showing notification');
+    console.log(JSON.stringify(notification));
+    if(notification == undefined)
+    {
+
+    }
+    else {
+      PushNotification.presentLocalNotification({
+        title: notification.title,
+        message: notification.body!,
+        vibrate: true,
+        vibration: 500,
+      });
+    }
+  };
+
+  const getToken = () => {
+    firebase
+      .messaging()
+      .getToken(firebase.app().options.messagingSenderId)
+      .then(x => console.log(x))
+      .catch(e => console.log(e));
+  };
+
+  getToken();
+  if (Platform.OS === 'ios') {
+    registerForRemoteMessages();
+  } else {
+    onMessage();
+  }
+
+  // Before we show the app, we have to wait for our state to be ready.
+  // In the meantime, don't render anything. This will be the background
+  // color set in native by rootView's background color. You can replace
+  // with your own loading component if you wish.
+  if (!rootStore) return null
+
+  // otherwise, we're ready to render the app
+  return (
+    <ToggleStorybook>
+      <RootStoreProvider value={rootStore}>
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <RootNavigator
+            ref={navigationRef}
+            initialState={initialNavigationState}
+            onStateChange={onNavigationStateChange}
+          />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </SafeAreaProvider>
+      </RootStoreProvider>
+    </ToggleStorybook>
+  )
+}
+
+export default App
